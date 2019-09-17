a48f4c36b747ddfbd1bf4d9c9ef254f0e3fc5d0c
Al Mamun Khan <almamunkhan04@gmail.com>
2019-09-17 21:52:19 +0600

    add server listen add and configure backend package.json

--- backend/server.js
+++ backend/server.js
@@ -3,9 +3,10 @@ const http = require("http");
 const socketIo = require("socket.io");
 const axios = require("axios");
 const port = process.env.PORT || 4001;
-const index = require("./routes/index");
+const index = require("./route/index");
 const app = express();
 app.use(index);
 const server = http.createServer(app);
 const io = socketIo(server); // < socket!
 const getApiAndEmit = "TODO"
+server.listen(port, () => console.log(`Listening on port ${port}`));
