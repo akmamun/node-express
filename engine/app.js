b2a614375e6aeff0911c99fa1b154e7aeae21d16
mrxmamun <almamunkhan04@gmail.com>
2020-03-11 15:00:05 +0600

    refactor

--- engine/app.js
+++ engine/app.js
@@ -1,4 +1,4 @@
-const createError = require("http-errors");
+// const createError = require("http-errors");
 const express = require("express");
 const path = require("path");
 const cookieParser = require("cookie-parser");
@@ -8,21 +8,17 @@ const indexRouter = require("./routes/index");
 
 const app = express();
 
-// view engine setup
-// app.set('views', path.join(__dirname, 'views'));
-// app.set('view engine', 'jade');
-
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-// app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  res
+app.use(async (req, res, next) => {
+  await res
     .status(404) // HTTP status 404: NotFound
     .send({ message: "404 Route Not found" });
 });
